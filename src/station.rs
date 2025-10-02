f3e6a5bab1c896c36c90d4641d432e12a0428d65
Abraham Zukor <abe@maticrobots.com>
2025-10-02 13:21:36 -0700

    Add `SignalLevelAgent`

--- src/station.rs
+++ src/station.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{cmp::Reverse, collections::HashMap, sync::Arc};
 
 use zvariant::{OwnedObjectPath, Value};
 
@@ -11,6 +11,9 @@ use crate::{
     },
     network::Network,
 };
+
+use signal_level_agent::SignalLevelAgentManager;
+pub mod signal_level_agent;
 
 #[derive(Debug, Clone)]
 pub struct Station {
@@ -93,6 +96,35 @@ impl Station {
             .collect();
 
         Ok(networks)
+    }
+
+    /// Register the agent object to receive signal strength level change notifications on the provided agent.
+    /// The "levels" parameters decides the thresholds in dBm that will generate a call to the
+    /// [`SignalLevelAgent::changed`] method whenever current RSSI crosses any of the values.  The number and distance between
+    /// requested threshold values is a compromise between resolution and the frequency of system wakeups and
+    /// context-switches that are going to be occurring to update the client's signal meter.  Only one agent
+    /// can be registered at any time.
+    pub async fn register_signal_level_agent(
+        &self,
+        mut levels: Vec<i16>,
+        agent: impl signal_level_agent::SignalLevelAgent,
+    ) -> zbus::Result<SignalLevelAgentManager> {
+        // Signal level boundaries should be sorted
+        levels.sort_by_key(|signal_level| Reverse(*signal_level));
+
+        let interface = signal_level_agent::SignalLevelInterface {
+            agent,
+            connection: self.connection.clone(),
+            levels: levels.clone(),
+        };
+
+        let manager = SignalLevelAgentManager::register_agent(self.clone(), interface).await?;
+
+        let proxy = self.proxy().await?;
+        proxy
+            .call_method("RegisterSignalLevelAgent", &(&manager.dbus_path, levels))
+            .await?;
+        Ok(manager)
     }
 }
 
